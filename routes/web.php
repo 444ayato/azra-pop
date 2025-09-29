eed40557dea4df3214ab6ad939ef0a5224b02021
444ayato <ivana24si@mahasiswa.pcr.ac.id>
2025-09-29 10:11:32 +0700

    submit form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,17 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
 use App\Http\Controllers\HomeController;
-
 use App\Http\Controllers\MahasiswaController;
-
 use App\Http\Controllers\PegawaiController;
-
+use App\Http\Controllers\QuestionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/pcr', function () {
     return 'Selamat Datang di Website Kampus PCR!';
 });
-
 
 Route::get('/mahasiswa', function () {
     return 'Halo Mahasiswa';
@@ -26,18 +20,20 @@ Route::get('/mahasiswa', function () {
 Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
 
 Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama saya: '.$param1;
+    return 'Nama saya: ' . $param1;
 });
 
-
 Route::get('/nim/{param1?}', function ($param1 = '') {
-    return 'NIM saya: '.$param1;
+    return 'NIM saya: ' . $param1;
 });
 
 Route::get('/about', function () {
     return view('halaman-about');
 });
 
-Route::get('/home',[HomeController::class,'index']);
+Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/pegawai', [PegawaiController::class, 'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+    ->name('question.store');
