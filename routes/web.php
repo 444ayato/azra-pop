11be4c37e96d2742b4920308a2b76113c143a984
444ayato <ivana24si@mahasiswa.pcr.ac.id>
2025-10-06 09:56:19 +0700

    menambahkan volt template

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\PegawaiController;
 use App\Http\Controllers\QuestionController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MahasiswaController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -37,3 +38,6 @@ Route::get('/pegawai', [PegawaiController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
     ->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
